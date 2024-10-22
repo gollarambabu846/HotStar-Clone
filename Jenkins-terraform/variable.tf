fbf465544b2ea7dcfb70bb20c5bf42e4cae667c7
Pavan M <132834477+Pavangowda73@users.noreply.github.com>
2024-10-22 16:55:43 +0530

    Create variable.tf

---
+++ Jenkins-terraform/variable.tf
@@ -0,0 +1,11 @@
+variable "ec2-ssh" {
+  description = " ssh for ec2 instance"
+}
+
+variable "ami-value"{
+    description = "ami id"
+}
+
+variable "instance-type" {
+  description = "instance type"
+}
