95bdcc9541e92e58b39c159aae5d5522fd0aef97
Golla Rambabu <116589566+gollarambabu846@users.noreply.github.com>
2024-12-15 22:38:17 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "s3myhotstar2024" # Replace with your actual S3 bucket name
+    bucket = "myhotstarbucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
